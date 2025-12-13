7740963b18a9cf1f44686e8263b193e0a41b8124
ronaldbaruelo <rcbaruelo1029@gmail.com>
2025-12-14 12:24:49 +1300

    Switch from bcrypt to bcryptjs for Vercel compatibility

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const session = require('express-session')
 const conn = require('./dbConfig')
 
